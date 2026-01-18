d0045a2dd8affc926124e0a029b23de224fae178
Pranjal Kumar Verma <pranjalverma975@gmail.com>
2026-01-19 00:01:03 +0530

    fixed postgrssql connection issue

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,20 +1,14 @@
 import pkg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
+
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+console.log("DB URL USED:", process.env.DATABASE_URL);
 
-pool.on("error", (err) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
